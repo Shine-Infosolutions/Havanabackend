25744825bf33e56312a6b73a181a7c33df947673
Sayain-Aditya <as4316664@gmail.com>
2025-12-22 16:55:56 +0530

    dfs

--- server.js
+++ server.js
@@ -51,13 +51,7 @@ const allowedOrigins = [
 
 app.use(
   cors({
-    origin: function (origin, callback) {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(null, false);
-      }
-    },
+    origin: "*",
     credentials: true,
     optionsSuccessStatus: 204,
   })
