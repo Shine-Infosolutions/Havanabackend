f011bbadd6769f6446b128dc88171196e9c43c0a
Sayain-Aditya <as4316664@gmail.com>
2025-12-22 13:48:01 +0530

    asd

--- server.js
+++ server.js
@@ -69,17 +69,13 @@ app.use(
 );
 app.use(express.json({ limit: "50mb" }));
 
-// Block socket.io requests FIRST - before any other routes
-app.use('/socket.io', (req, res) => {
-  res.end();
-});
-
-app.all("/socket.io*", (req, res) => {
-  res.end();
-});
-
-app.get("/socket.io/", (req, res) => {
-  res.end();
+// Block ALL socket.io requests immediately
+app.use((req, res, next) => {
+  if (req.url.includes('socket.io')) {
+    res.status(404).json({ error: 'Socket.IO not supported' });
+    return;
+  }
+  next();
 });
 
 // Serve uploaded files for fallback method
